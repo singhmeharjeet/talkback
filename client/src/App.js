eb975a4e870097a8686585837f7a34b013bbfebd
Meharjeet Singh <meharjeet_singh@sfu.ca>
2022-08-26 10:44:37 -0700

    added the landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css"
 import Join from "./components/Join/Join.jsx"
 function App() {
 	return (
--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -2,7 +2,38 @@ import React from "react";
 import "./Join.css";
 
 function Join() {
-	return <div>Join</div>;
+  const handleJoin = (e) => {
+    console.log("func called");
+		/**
+		 * TODO: (1) generate uuid (2) store info in local storage (3) navigate to all rooms page
+		 */
+	};
+	return (
+		<>
+			<div className="screen-wrapper">
+				<div className="screen-wrapper-title">
+					Welcome to <br style={{ width: "0" }} /> TalkBack
+				</div>
+				{/* <div className="screen-wrapper-sub-title">Find the debate of your choice</div> */}
+				<div className="screen-wrapper-input">
+					<input
+						type="text"
+						placeholder="Enter your name"
+						required
+						autoFocus
+						onKeyDown={(e) =>
+							e.key == "Enter" ? handleJoin() : null
+						}
+					/>
+				</div>
+				<div className="screen-wrapper-button">
+					<button type="button" onClick={handleJoin}>
+						Join Debate
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Join;
