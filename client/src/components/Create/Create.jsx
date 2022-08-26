ce91099a688e118c59f35fc1b8529dacb1e4bf31
Rara212 <78282415+Rara212@users.noreply.github.com>
2022-08-27 06:39:48 +0700

    added radio button

--- client/src/components/Create/Create.jsx
+++ client/src/components/Create/Create.jsx
@@ -1,30 +1,59 @@
 import React from "react";
+import { useState } from "react";
 import { getLocalStorage } from "../utils";
 import "./Create.css";
 
 function Create() {
+    const [motion, setMotion] = useState('');
+
+    const onChange = (e) => {
+        const { value } = e.target;
+
+        setMotion(value); 
+    }
+
+    const showData = () => {
+        console.log('Motion: ', motion);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+
   return (
     <>
         <div className="list-wrapper">
             <div className="AppGlass" style={{ height: "auto" }}>
                 <h1 className="list-title">Create Chambers!</h1>
-                <div className="list-search">
-						<input type="text" placeholder="Search Topic..." />
-				</div>
-                <input 
-                    type="radio"
-                    name="position-radio-btn"
-                    value="Opposition"
-                    // checked={isRadioSelected("Opposition")}
-                    // onChange={handleRadioClick}
-                />
-                <input 
-                    type="radio"
-                    name="position-radio-btn"
-                    value="Affirmative"
-                    // checked={isRadioSelected("Affirmative")}
-                    // onChange={handleRadioClick}
-                />
+                <form onSubmit={onSubmit}>
+                    <div className="list-search">
+                            <input onChange={onChange} name="motion" value={motion} type="text" placeholder="Type your motion here..." />
+                    </div>
+                    <div className="position-buttons">
+                        <label>
+                        <input 
+                            type="radio"
+                            name="position-radio-btn"
+                            value="Opposition"
+                            // checked={isRadioSelected("Opposition")}
+                            // onChange={handleRadioClick}
+                        />Opposition
+                        </label>
+                        <label>
+                        <input 
+                            type="radio"
+                            name="position-radio-btn"
+                            value="Affirmative"
+                            // checked={isRadioSelected("Affirmative")}
+                            // onChange={handleRadioClick}
+                        />Affirmative
+                        </label>
+                        
+					</div>
+                    <button>Create Chamber</button>
+                </form>
+                
             </div>
         </div>
     </>
